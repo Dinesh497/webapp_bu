406e68cad00697fe679e58387bd2d5674aa40639
Dinesh497 <dinesh.bhagwandin@hva.nl>
2014-11-26 12:36:02 +0100

    db

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 
 	$database = "webdb";
 
-	$dbcon = mysql_connect("107.189.33.148:3306", "root", "P@ssw0rd") or die (" could not connect to database");
+	$dbcon = mysql_connect("107.189.33.148:3306", "Beheerder", "P@ssw0rd") or die (" could not connect to database");
 	$selectdb = mysql_select_db($database, $dbcon);
 	
 	$gebruiker = $_POST['username'];
