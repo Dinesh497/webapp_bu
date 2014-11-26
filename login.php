635b5fdd58b5cee5c883944af8eaa4ea3cebbfdc
Dinesh497 <dinesh.bhagwandin@hva.nl>
2014-11-26 14:02:08 +0100

    `sdada

--- login.php
+++ login.php
@@ -1,6 +1,8 @@
 <?php
 	session_start(); // Starting Session
 
+	
+
 	$dbcon = mysql_connect("localhost", "Beheerder", "P@ssw0rd") or die ("could not connect to database");
 	$selectdb = mysql_select_db("webdb", $dbcon);
 	
