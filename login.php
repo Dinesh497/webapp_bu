0eca6ce30a6311ad100d43466e5f21a076ca7104
Dinesh497 <dinesh.bhagwandin@hva.nl>
2014-11-26 13:54:06 +0100

    asddd

--- login.php
+++ login.php
@@ -1,12 +1,7 @@
 <?php
 	session_start(); // Starting Session
 
-	$hostname = "localhost";
-	$database = "webdb";
-	$username = "Beheerder";
-	$password = "P@ssword"
-
-	$dbcon = mysql_connect("107.189.33.148:3306", "Beheerder", "P@ssw0rd") or die (" could not connect to database");
+	$dbcon = mysql_connect("107.189.33.148:3306", "Beheerder", "P@ssw0rd") or die ("could not connect to database");
 	$selectdb = mysql_select_db("webdb", $dbcon);
 	
 	$gebruiker = $_POST['username'];
