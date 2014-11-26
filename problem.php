0a5cb3d596d41d67abcfa83dfcaade6be22c2465
Dinesh497 <dinesh.bhagwandin@hva.nl>
2014-11-26 15:23:04 +0100

    problem

--- problem.php
+++ problem.php
@@ -35,7 +35,7 @@ include ("session.php");
 </tr>
 <tr>
  <td valign="top">
-  <label for="first_name">First Name *</label>
+  <label for="first_name">First Name </label>
  </td>
  <td valign="top">
   <input  type="text" name="first_name" maxlength="50" size="30">
@@ -44,7 +44,7 @@ include ("session.php");
  
 <tr>
  <td valign="top">
-	<label for="last_name">Last Name *</label>
+	<label for="last_name">Last Name </label>
  </td>
  <td valign="top">
   <input  type="text" name="last_name" maxlength="50" size="30">
@@ -53,7 +53,7 @@ include ("session.php");
 
 <tr>
  <td valign="top">
-  <label for="email">Email Address *</label>
+  <label for="email">Email Address </label>
  </td>
  <td valign="top">
   <input  type="text" name="email" maxlength="80" size="30">
@@ -70,7 +70,7 @@ include ("session.php");
 </tr>
 <tr>
  <td valign="top">
-  <label for="comments">Comments *</label>
+  <label for="comments">Descprition</label>
  </td>
  <td valign="top">
   <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
