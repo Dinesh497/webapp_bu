736be2626c8c6fd5da621fee6a30260c99ce13c1
JLee003 <joh.lee390@gmail.com>
2014-12-03 12:31:55 +0100

    ssadasd

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
 <link rel='stylesheet' href='style.css' />
-<link rel="stylesheet" media="(max-width: 640px)" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link rel="stylesheet" media="(max-width: 400px)" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 
 <body>
  
