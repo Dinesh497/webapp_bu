535520e09a01b50f1697bb999ee77b12d1ff6173
JLee003 <joh.lee390@gmail.com>
2014-12-08 14:10:30 +0100

    lol
    lol

--- submit.php
+++ submit.php
@@ -11,4 +11,10 @@
 	$KP = $_POST['Known problems'];
 	$description = $_POST['description'];
 	$handledate = $_POST['handle date'];
+	
+	
+	// room map moet er nog bij
+	$sSQL = 'INSERT INTO tickets (ticket_id, user, status, priority, location, room_type, known_problems, description, handle_before, date) 
+VALUES (\'' . $ticket .'\', \'' . $user .'\', \'' . $status .'\', \'' . $priority .'\', \'' . $location .'\', \'' . $room .'\', \'' . $KP .'\', \'' . $description .'\',\'' . $handledate .'\', \'' . $date .'\')';
+						 
 ?>
