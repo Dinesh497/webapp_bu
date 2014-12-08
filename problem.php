47235c346801cb4dbb7daf640297d29acb4a86e4
JLee003 <joh.lee390@gmail.com>
2014-12-08 12:34:19 +0100

    dasd
    adasd

--- problem.php
+++ problem.php
@@ -46,7 +46,7 @@ include ("session.php");
 	<td><script>
 	var tD = new Date();
 	var datestr = tD.getDate() + "/" + (tD.getMonth()+ 1) + "/" + tD.getFullYear();
-	document.write("<input type='text' name='textField' value='"+datestr+"'>");
+	document.write("name='date' value='"+datestr+"'>");
 	</script></td>
 	</tr>
 	<tr>
