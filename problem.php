d2bd389cc4b4c3d76258d952ee894f0eb311bebd
Dinesh497 <dinesh.bhagwandin@hva.nl>
2015-01-07 13:45:43 +0100

    problem

--- problem.php
+++ problem.php
@@ -87,8 +87,17 @@ include ("session.php");
 	</tr>
 	<tr>
 	<td>Location :</td>
-	<td><input name="location" type="text">
-		<a href='problem.php?action=edit&id='>Get Map</a>
+	<td><select name="location"  type="text">
+		<td><button input type="submit" class="pure-button pure-button-primary">Get Map</button></td>
+		<option value=""></option>
+		<?php 
+		$rooms = mysql_query("SELECT room_id FROM rooms", $dbcon) or die (mysql_error());
+		while($row = mysql_fetch_array($rooms)){
+
+			echo "<option value=" . $row['room_id'] . ">" . $row['room_id'] . "</option>";
+			}
+		?>
+	</select>
 	</td>
 	</tr>
 	<tr>
