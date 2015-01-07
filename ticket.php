8eec22539f9031ef151182f52e07800fb37b83e2
JLee003 <joh.lee390@gmail.com>
2015-01-07 14:41:09 +0100

    form

--- ticket.php
+++ ticket.php
@@ -44,7 +44,96 @@ $ticket = $_GET['ticket'];
 $result = mysql_query("SELECT * FROM tickets WHERE id=$ticket", $dbcon);
 	while($row = mysql_fetch_array($result)){
 		
-		
+	echo "<form name="submitform"  action="submit.php" method="POST">";
+	
+	echo "<table class='submittable'  >";
+	echo "<tr>";
+	
+	echo "</tr>";
+	echo "<tr>";
+	echo "<td>Ticket ID :</td>";
+	echo "<td> " . $row['id'] . "</td>";
+	echo "</tr>";
+	echo "<tr>";
+	echo "<td>Created on :</td>";
+	echo "</tr>";
+	echo "<tr>";
+	echo "<td>Created by :</td>";
+	echo "<td> " . $row['user'] . "</td>";
+	echo "</tr>";
+	echo "<tr>";
+	echo "<td>status :</td>"
+	echo "<td>";
+	echo "<select name=""status"">";
+	echo "<option value=" . $row['status'] . ">" . $row['status'] . "</option>";
+
+	echo "<option value=""pending">"Pending""</option>";
+	echo "<option value=""closed">"Closed""</option>";
+	echo "</select>"
+	//</td>
+	//</tr>
+	//<tr>
+	//<td>Priority :</td>
+	//<td>
+	//<select name="priority">
+	//<option value=""></option>
+	//<option value="High">High</option>
+	//<option value="Medium">Medium</option>
+	//<option value="Low">Low</option>
+	//</select>
+	//</td>
+	//</tr>
+	//<tr>
+	//<td>Location :</td>
+	//<td><select name="location"  type="text">
+	//<option value=""></option>
+
+	//</select>
+	//<input type="submit" a href='problem.php?action=edit&id='>Get Map</a>
+	//</td>
+	//</tr>
+	//<tr>
+	//<td>Room type :</td>
+	//<td><input name="room" type="text"></td>
+	//</tr>
+	//<tr>
+	//<td>Known Problems :</td>
+	//<td>
+	//<select name="known">
+	//<option value=""></option>
+	//<option value="lamp">Lamp</option>
+	//<option value="mini-bar">minibar</option>
+	//<option value="remote control">Remote control</option>
+	//<option value="Television">Television</option>
+	//<option value="sink">Sink</option>
+	//<option value="shower">Shower</option>
+	//<option value="bathtub">Bathtub</option>
+	//<option value="window">Window</option>
+	//<option value="chair/couch">Chair/couch</option>
+	//</select>
+	//</tr>
+	//<tr>
+	//<td>
+	//Description:
+	//</td>
+	//<td>
+	//<textarea  name="description" maxlength="1000" cols="25" rows="6"></textarea>
+	//</td>
+	//</tr>
+	//<tr>
+	//<td>Handle before :</td>
+	//<td><input type="date" name="hdate" value=""  /></td>
+	//</tr>
+	//<tr>
+	//<td>Room map</td>
+	//<td>
+	//</tr>
+
+	
+	//<td><button input type="submit" class="pure-button pure-button-primary">Submit</button></td>
+	//</table>
+	
+</form>
 		echo "<br>";
 		echo "<tr>";
 		echo "<td> " . $row['id'] . "</td>";
@@ -58,7 +147,7 @@ $result = mysql_query("SELECT * FROM tickets WHERE id=$ticket", $dbcon);
 	
 		echo "</tr>";
 	
-		echo "</form>";
+		
 	}
 
 ?>
