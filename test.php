64df05e19fe58e3688d8cab951c2ac6f8ec49ccc
JLee003 <joh.lee390@gmail.com>
2015-01-07 09:55:47 +0100

    kuthub sucks

--- test.php
+++ test.php
@@ -37,7 +37,57 @@ include ('connection.php');
 	</td>
 </form>
 </tr>
+<?php
 
+echo "<table class='tasktable'>";
+
+echo "<tr>";
+echo "<td>Ticket ID: <b></td>";
+echo "<td>Created by: <b></td>";
+echo "<td>Priority:</td>";
+echo "<td>Location:</td>";
+echo "<td>Known_problem</td>";
+echo "<td>Description</td>";
+echo "<td>Handle before</td>";
+echo "<td>Handle before</td>";
+
+$result = mysql_query("SELECT * FROM tickets WHERE status='open'", $dbcon);
+	while($row = mysql_fetch_array($result)){
+		
+	
+	
+			
+	
+			echo "<br>";
+			
+			echo "<tr>";
+				echo "<td><input name="username" type="text" id="username"> " . $row['ticket_id'] . "</td>";
+			
+			
+				echo "<td> " . $row['user'] . "</td>";
+			
+				echo "<td> " . $row['priority'] . "</td>";
+			
+				echo "<td>" . $row['location'] . "</td>";
+			
+			
+			
+				
+			
+				echo "<td>" . $row['known_problems'] . "</td>";
+			
+				echo "<td> " . $row['description'] . "</td>";
+		
+				echo "<td>" . $row['handle_before'] . "</td>";
+			
+				
+				
+			echo "</tr>";	
+			
+			
+}
+echo "</table>";//tabel sluiten
+?>
 
 
 
