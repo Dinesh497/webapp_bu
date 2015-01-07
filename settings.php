6a8606f86e1e5e7c7a54b575fefeb6ac949be0a1
Dinesh497 <dinesh.bhagwandin@hva.nl>
2015-01-07 14:01:24 +0100

    asd

--- settings.php
+++ settings.php
@@ -44,7 +44,7 @@ include ("session.php");
 			}
 		?>
 	</select>
-	<input type="submit" ><a href='settings.php?action=edit&id='>Get Map</a>
+	<type="submit" ><a href='settings.php?action=edit&id='>Get Map</a>
 	<button input type="submit" class="pure-button pure-button-primary">Get Map</button>
 	</td>
 </tr>
