dcb4bb7a04cf033da4173834608c1129ad4aaf36
JLee003 <joh.lee390@gmail.com>
2015-01-08 15:00:17 +0100

    xzx

--- editticket.php
+++ editticket.php
@@ -3,27 +3,22 @@
 	require ("connection.php");
 
 	
-	$ticket = $_POST['ticket'];
-	$date = $_POST['date'];
-	$user = $_SESSION['gebruiker'];
+
 	$status = $_POST['status'];
 	$priority = $_POST['priority'];
-	$location = $_SESSION['room_id'];
-	$room = $_POST['room'];
-	$KP = $_POST['known'];
 	$description = $_POST['description'];
-	$handledate = $_POST['hdate'];
+
 	
 	
 	// room map moet er nog bij
-	$sSQL = 'INSERT INTO tickets (ticket_id, user, status, priority, location, room_type, known_problems, description, handle_before, date) 
-	VALUES (\'' . $ticket .'\', \'' . $user .'\', \'' . $status .'\', \'' . $priority .'\', \'' . $location .'\', \'' . $room .'\', \'' . $KP .'\', \'' . $description .'\',\'' . $handledate .'\', \'' . $date .'\')';
+	$sSQL = 'INSERT INTO tickets (status, priority, description) 
+	VALUES (\'' . $status .'\', \'' . $priority .'\', \'' . $description .'\')';
 					
 @mysql_query($sSQL);
 	if(!mysql_error())
 						{
 						
-							echo '<br><br><br>You are now registerd.';
+							echo '<br><br><br>Ticket is updated.';
 							
 						}
 						else
