eabb075c7c4688b084aeb6eba3faba8b2d3309b7
Dinesh497 <dinesh.bhagwandin@hva.nl>
2015-01-08 15:18:07 +0100

    tickets1

--- submit.php
+++ submit.php
@@ -16,7 +16,7 @@
 	
 	
 	// room map moet er nog bij
-	$sSQL = 'INSERT INTO tickets (ticket_id, user, status, priority, location, room_type, known_problems, description, handle_before, date) 
+	$sSQL = 'INSERT INTO tickets1 (ticket_id, user, status, priority, location, room_type, known_problems, description, handle_before, date) 
 	VALUES (\'' . $ticket .'\', \'' . $user .'\', \'' . $status .'\', \'' . $priority .'\', \'' . $location .'\', \'' . $room .'\', \'' . $KP .'\', \'' . $description .'\',\'' . $handledate .'\', \'' . $date .'\')';
 					
 @mysql_query($sSQL);
