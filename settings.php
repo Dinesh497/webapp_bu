68fcb15c3a8d39d5b5643696b602a33e36745a88
Dinesh497 <dinesh.bhagwandin@hva.nl>
2015-01-12 09:53:37 +0100

    test again

--- settings.php
+++ settings.php
@@ -76,21 +76,21 @@ include ("session.php");
 
 	echo '<img src="data:image/jpeg;base64,' . base64_encode( $row['room_map'] ) . '" />';
 
-	/*
+	
 	$im = imagecreatefromjpeg($row['room_map']);
 
 	$ellipseColor = ImageColorAllocate($im, 0, 0, 255); 
 	
-	//$start_x = 10; 
-	//$start_y = 20;
-	//$font = 'arial.ttf';
-	//Imagettftext($im, 12, 0, $start_x, $start_y, $black, $font, 'text to write'); 
+	$start_x = 10; 
+	$start_y = 20;
+	$font = 'arial.ttf';
+	Imagettftext($im, 12, 0, $start_x, $start_y, $black, $font, 'text to write'); 
 
 	imagefilledellipse($im, 100, 100, 10, 10, $ellipseColor)
 
 	header('Content-Type: image/jpeg');
 	Imagejpeg($im, '', 100); 
-	*/
+	
 
 	?>
 </tr>
