58c5ba1f2de5ed49abae89a24c6757290f0e6b49
JLee003 <joh.lee390@gmail.com>
2015-01-12 12:39:36 +0100

    asdad

--- task.php
+++ task.php
@@ -22,9 +22,56 @@ include ('connection.php');
 	This query will take effect for any screen smaller than 760px
 	and also iPads specifically.
 	*/
+	@media 
+	only screen and (max-width: 760px),
+	(min-device-width: 768px) and (max-device-width: 1024px)  {
 	
-	
+		/* Force table to not be like tables anymore */
+		.tasktable, thead, tbody, th, td, tr { 
+			display: block; 
+		}
 		
+		/* Hide table headers (but not display: none;, for accessibility) */
+		thead tr { 
+			position: absolute;
+			top: -9999px;
+			left: -9999px;
+		}
+		
+		tr { border: 1px solid #ccc; }
+		
+		td { 
+			/* Behave  like a "row" */
+			border: none;
+			border-bottom: 1px solid #eee; 
+			position: relative;
+			padding-left: 50%; 
+		}
+		
+		td:before { 
+			/* Now like a table header */
+			position: absolute;
+			/* Top/left values mimic padding */
+			top: 6px;
+			left: 6px;
+			width: 45%; 
+			padding-right: 10px; 
+			white-space: nowrap;
+		}
+		
+		/*
+		Label the data
+		*/
+		td:nth-of-type(1):before { content: "Ticket ID"; }
+		td:nth-of-type(2):before { content: "Created By"; }
+		td:nth-of-type(3):before { content: "Priority"; }
+		td:nth-of-type(4):before { content: "Location"; }
+		td:nth-of-type(5):before { content: "Status"; }
+		td:nth-of-type(6):before { content: "Description"; }
+		td:nth-of-type(7):before { content: "Handle before"; }
+		td:nth-of-type(8):before { content: "Details"; }
+		
+	}
 	
 	/* Smartphones (portrait and landscape) ----------- */
 	@media only screen
@@ -36,7 +83,12 @@ include ('connection.php');
 			width: 320px; }
 		}
 	
-	
+	/* iPads (portrait and landscape) ----------- */
+	@media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
+		body { 
+			width: 495px; 
+		}
+	}
 	
 	</style>
 <body>
