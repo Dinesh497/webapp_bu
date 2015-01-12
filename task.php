a8c9fa19d8fa3d873003e7eaed180c801857f37c
JLee003 <joh.lee390@gmail.com>
2015-01-12 11:05:50 +0100

    test

--- task.php
+++ task.php
@@ -88,9 +88,10 @@ $result = mysql_query("SELECT * FROM tickets1 WHERE status='$input'", $dbcon);
 		</tr>
 		</tbody>
 		</form>
+		</table>
 		<?php
 	}
-echo "</table>";//tabel sluiten
+
 ?>
 
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
