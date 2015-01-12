80c8fc76a06f9b4b3950e15ae97ececc303a20bb
JLee003 <joh.lee390@gmail.com>
2015-01-12 23:56:21 +0100

    xcxc

--- homepage.php
+++ homepage.php
@@ -11,17 +11,57 @@ include ("session.php");
 <title>Westcord Fashion Hotel</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
-<link rel='stylesheet' href='style.css' />
+<link rel='stylesheet' href='teststyle.css' />
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400,700,400italic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
 
+<link rel="stylesheet" href="css/navstyle.css" />
+<script src="js/jquery-1.9.1.min.js"></script>
+<script src="js/modernizr.custom.js"></script>
 
+<script>
+    $(document).ready(function(){
+        $("#nav-mobile").html($("#nav-main").html());
+        $("#nav-trigger span").click(function(){
+            if ($("nav#nav-mobile ul").hasClass("expanded")) {
+                $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+                $(this).removeClass("open");
+            } else {
+                $("nav#nav-mobile ul").addClass("expanded").slideDown(250);
+                $(this).addClass("open");
+            }
+        });
+    });
+</script>
 <body>
 
 
 
  
 
-<tr>
+<div id="main">
+    <div class="container1">
+        <div id="nav-trigger">
+            <span>Menu</span>
+        </div>
+        <nav id="nav-main">
+            <ul>
+                <li><a href="homepage.php">Home</a></li>
+                <li><a href="location.php">Add Task</a></li>
+				<li><a href="task.php">Check task</a></li>
+                <li><a href="settings.php">settings</a></li>
+                <li><a href="logout.php">logout</a></li>
+                
+            </ul>
+        </nav>
+        <nav id="nav-mobile"></nav>
+
+        <section>
+        
+        </section>
+    </div>
+</div>
 <form name="form1"  action="logout.php" method="POST">
 	<td>
 	<table class='logout' >
