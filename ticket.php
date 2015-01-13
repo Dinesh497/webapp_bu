45fd7c0bf09e7c8f6d215f8c1c18ed096daa8f93
JLee003 <joh.lee390@gmail.com>
2015-01-13 21:08:43 +0100

    ssdx

--- ticket.php
+++ ticket.php
@@ -14,7 +14,7 @@ $ticket = $_GET['ticket'];
 <title>Westcord Fashion Hotel</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
-<link rel='stylesheet' href='style.css' />
+<link rel='stylesheet' href='teststyle.css' />
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 
 
