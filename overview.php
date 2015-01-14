b12b5e3673801869c719b3746626d49f5d99edc9
Dinesh497 <dinesh.bhagwandin@hva.nl>
2015-01-14 12:10:21 +0100

    qwe

--- overview.php
+++ overview.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!--<!doctype html>-->
 <?php
 include ("session.php");
 include ("nav.php");
@@ -10,12 +10,12 @@ require ('fpdf17/fpdf.php');
 
 
 ?>
-<title>Westcord Fashion Hotel</title>
+<!--<title>Westcord Fashion Hotel</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
 <link rel='stylesheet' href='style.css' />
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-<body>
+<body>-->
 
 <?php
 
@@ -27,6 +27,6 @@ $pdf->Output();
 
 ?>
 
-</body>
+<!--</body>
 
-</html>
+</html>-->
