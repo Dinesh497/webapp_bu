d3c225541c366559b60011fdb49f492411c4787c
JLee003 <joh.lee390@gmail.com>
2015-01-14 11:39:45 +0100

    as

--- login.php
+++ login.php
@@ -13,7 +13,7 @@
 	$query = mysql_query("SELECT * FROM users WHERE username='$gebruiker'");
 	$numrows = mysql_num_rows($query);
 	
-	echo $numrows;
+	
 	/*if(isset($gebruiker) && isset($passencrypt))
 	{
 		$_SESSION['gebruiker'] = mysql_fetch_assoc($result);
@@ -26,14 +26,14 @@
 	*/
 	
 //
-	//if($numrows!=0){
-		//header("location:lol.php");
+	if($numrows!=0){
+		header("location:lol.php");
 		//while ($row = mysql_fetch_assoc($result)){
 		//$dbusername = $row['username'];
 		//$dbpassword = $row['password'];
 		//echo $dbusername;
 		//echo $dbpassword;
-	//}
+	}
 		//if ($gebruiker==$dbusername&&$wachtwoord==$dbpassword){
 		//$_SESSION['gebruiker'] = mysql_fetch_assoc($result);
 		//$_SESSION['gebruiker'] = $gebruiker;
