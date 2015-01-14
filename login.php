6e5eea9eaf3afb96c31ba68fbf9ef41309d63693
Dinesh497 <dinesh.bhagwandin@hva.nl>
2015-01-14 10:40:32 +0100

    asd

--- login.php
+++ login.php
@@ -14,7 +14,7 @@
 	$result = mysql_query($query);
 	$count = mysql_num_rows($result);
 	
-
+	/*
 	if(!isset($count['username']) && !isset($count['password']))
 	{
 		$_SESSION['gebruiker'] = mysql_fetch_assoc($result);
@@ -24,15 +24,17 @@
 	} else{
 		echo 'Incorrect username or password';
 	}
+	*/
 
-	/*if(!$count==1){
+	if(!$count==1){
 		$_SESSION['gebruiker'] = mysql_fetch_assoc($result);
 		$_SESSION['gebruiker'] = $gebruiker;
-		$_SESSION['wachtwoord'] = $passencrypt;
+		echo $count;
+		echo $result;
 		header("location:homepage.php");
 	} else{
 		echo 'Incorrect username or password';
 	}
-	*/
+	
 	
 ?>
