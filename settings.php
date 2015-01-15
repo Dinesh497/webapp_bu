75e9542aa700df74336d3da646bca88f6ea18161
JLee003 <joh.lee390@gmail.com>
2015-01-15 14:40:21 +0100

    dasd

--- settings.php
+++ settings.php
@@ -13,7 +13,7 @@ include ("nav.php");
 <title>Westcord Fashion Hotel</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
-<link rel='stylesheet' href='teststyle.css' />
+<link rel='stylesheet' href='style.css' />
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 <body>
 
