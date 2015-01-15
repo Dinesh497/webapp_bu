03d32f65db9ea0f1800847b77f5c22b1dd12646b
Dinesh497 <dinesh.bhagwandin@hva.nl>
2015-01-15 14:31:24 +0100

    sd

--- overview.php
+++ overview.php
@@ -12,7 +12,7 @@ function LoadData($file)
     $lines = file($file);
     $data = array();
     foreach($lines as $line)
-        $data[] = explode(';',trim($line));
+        $data[] = explode('|',trim($line));
     return $data;
 }
 
