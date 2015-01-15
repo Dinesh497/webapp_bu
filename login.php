c395c91e1573a8b0e94c913d80070bac81818943
JLee003 <joh.lee390@gmail.com>
2015-01-15 14:47:26 +0100

    sadd

--- login.php
+++ login.php
@@ -3,8 +3,8 @@
 	require ('connection.php');
 
 	
-	$gebruiker = mysql_real_escape_string$_POST['username'];
-	$wachtwoord = mysql_real_escape_string$_POST['password'];
+	$gebruiker = mysql_real_escape_string $_POST['username'];
+	$wachtwoord = mysql_real_escape_string $_POST['password'];
 	
 	$gebruiker = stripslashes($gebruiker);
 	$wachtwoord = stripslashes($wachtwoord);
