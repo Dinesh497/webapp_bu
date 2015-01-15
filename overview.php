93886d2d35378832b5e2960bdd47b75a2cc16b93
Dinesh497 <dinesh.bhagwandin@hva.nl>
2015-01-15 11:40:49 +0100

    $I

--- overview.php
+++ overview.php
@@ -34,7 +34,7 @@ $pdf = new FPDF();
 $pdf->AliasNbPages();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
-for($1=1;$1<=40;$i++){
+for($i=1;$i<=40;$i++){
 	$pdf->Cell(0,10,'Printing line number '.$i,0,1);
 	}
 $pdf->Cell(50,10,'Hello World!');
