92305ffa3a412f268e7e0e3dfde2a2e9ae0b491c
Dinesh497 <dinesh.bhagwandin@hva.nl>
2015-02-03 14:11:16 +0000

    asd

--- sql_table_to_pdf/generate-pdf.php
+++ sql_table_to_pdf/generate-pdf.php
@@ -24,7 +24,7 @@ mysql_select_db('webdb');
 $pdf=new PDF();
 $pdf->AddPage();
 //First table: put all columns automatically
-$pdf->Table('SELECT `id`, `date`, `location`, `known_problems`, `handle_before`  from tickets1 where known_problems = "$input" order by `id`');
+$pdf->Table('SELECT `id`, `date`, `location`, `known_problems`, `handle_before`  from tickets1 where known_problems =$input order by `id`');
 //$pdf->Table($result);
 $pdf->AddPage();
 //Second table: specify 3 columns
